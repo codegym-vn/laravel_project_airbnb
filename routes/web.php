34dc82dca09387c9f685d92ac4e3fa8fb97802c5
letuan1 <le01627177611@gmail.com>
2018-10-31 13:38:46 +0700

    xua route

--- routes/web.php
+++ routes/web.php
@@ -4,45 +4,50 @@ Route::get('/', 'indexController@index')->name('index');
 
 Route::get('/new', 'indexController@new')->name('new');
 
-Route::get('/list-bock-house', 'HousesController@showHouses')->name('listBockHouse');
-
-Route::get('/show-updated-home-status/{id}', 'HousesController@showUpdatedHomeStatus')->name('showUpdatedHomeStatus');
-
 Route::get('/information-house', 'indexController@informationHouse')->name('information-house');
-
-Route::get('/sign-in', 'LoginController@signIn')->name('sign-in');
-
-Route::post('/search', 'HousesController@search')->name('search');
-// chưa có return về view nào sáng code
-
-Route::post('/updated-home-status', 'HousesController@updatedHomeStatus')->name('updatedHomeStatus');
 
 Route::get('/sign-in', 'LoginController@signIn')->name('sign-in');
 
 Route::post('/dashboard', 'LoginController@login')->name('dashboard');
 
 Route::get('/sign-up', 'LoginController@signUp')->name('sign-up');
- 
+
 Route::post('/sign-up', 'LoginController@register')->name('sign-up');
 
-Route::get('/dashboarduser', 'DashBoardController@dashBoardUser')->name('dashboardUser');
+Route::prefix('admin')->group(function () {
 
-Route::get('/user/edit-users/{id}', 'DashBoardController@editUser')->name('editUsers');
+    Route::get('/dashboarduser', 'DashBoardController@dashBoardUser')->name('dashboardUser');
 
-Route::post('/user/edit-user/{id}', 'DashBoardController@updateUser')->name('editUser');
+    Route::get('/user/edit-users/{id}', 'DashBoardController@editUser')->name('editUsers');
 
-Route::get('/user/forgot-pass/{id}', 'DashBoardController@forgotPassword')->name('forgot-pass');
+    Route::post('/user/edit-user/{id}', 'DashBoardController@updateUser')->name('editUser');
 
-Route::post('/user/forgot-password/{id}', 'DashBoardController@forgotPass')->name('forgot-password');
+    Route::get('/user/forgot-pass/{id}', 'DashBoardController@forgotPassword')->name('forgot-pass');
 
-Route::get('/calendar/{id}', 'UserController@showCalendar')->name('showCalendar');
+    Route::post('/user/forgot-password/{id}', 'DashBoardController@forgotPass')->name('forgot-password');
 
-Route::get('/create', 'CreatePostsController@create')->name('post.create');
+    Route::get('/create', 'CreatePostsController@create')->name('post.create');
 
-Route::post('/create', 'CreatePostsController@store')->name('post.store');
+    Route::post('/create', 'CreatePostsController@store')->name('post.store');
 
-Route::get('/seeDetails/{id}', 'HousesController@seeDetails')->name('seeDetails');
+})->middleware('auth');
 
-Route::get('/show-statistics/{id}', 'UserController@showStatistics')->name('showStatistics');
+Route::prefix('/user')->group(function () {
 
-Route::post('/feedback/{id}', 'UserController@feedback')->name('feedback');
+    Route::get('/seeDetails/{id}', 'HousesController@seeDetails')->name('seeDetails');
+
+    Route::get('/list-bock-house', 'HousesController@showHouses')->name('listBockHouse');
+
+    Route::get('/show-statistics/{id}', 'UserController@showStatistics')->name('showStatistics');
+
+    Route::post('/feedback/{id}', 'UserController@feedback')->name('feedback');
+
+    Route::get('/calendar/{id}', 'UserController@showCalendar')->name('showCalendar');
+
+    Route::post('/updated-home-status', 'HousesController@updatedHomeStatus')->name('updatedHomeStatus');
+
+    Route::post('/search', 'HousesController@search')->name('search');
+
+    Route::get('/show-updated-home-status/{id}', 'HousesController@showUpdatedHomeStatus')->name('showUpdatedHomeStatus');
+
+});
